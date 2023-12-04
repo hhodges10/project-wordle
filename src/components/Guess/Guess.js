b21fcfe98128acaa32c4fee43c359c73191f1742
Haley Hodges <hhodges10@msn.com>
2023-12-03 18:30:25 -0600

    ex4: adding logic to determine cell classes based on correct and incorrect letters

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const statusArr = checkGuess(value, answer);
+
+  function setClassName(num) {
+    return `cell ${statusArr[num].status}`;
+  }
+
   return (
     <div className="guess-results">
       <p className="guess">
         {range(5).map((num) => (
-          <span className="cell" key={num}>
+          <span className={value ? setClassName(num) : 'cell'} key={num}>
             {value ? value[num] : undefined}
           </span>
         ))}
--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -5,11 +5,11 @@ import Guess from '../Guess';
 import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
-function PreviousGuesses({ guessList }) {
+function PreviousGuesses({ guessList, answer }) {
   return (
     <div className="guess-results">
       {range(0, NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess key={num} value={guessList[num]} />
+        <Guess key={num} value={guessList[num]} answer={answer} />
       ))}
     </div>
   );
